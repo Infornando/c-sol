e7ac7ad7c6205eb3a0e553e65c29488b7fd940d1
Eric <Eric@ERIC-PC>
2019-03-26 21:01:36 -0500

    Add project files.

---
+++ c/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, sum;
+	
+	cout << "first number ";
+	cin >> a;
+	cout << "second number ";
+	cin >> b;
+	sum = a + b;
+	cout << "The sum is " << sum << endl;
+	system("pause");
+	return 0;
+}
